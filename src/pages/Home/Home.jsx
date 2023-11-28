5d8f2b00d220b9f8141a4755bf16600177636886
Atif Furqaan <atifurqaan@gmail.com>
2023-11-28 22:31:47 +0000

    Second commit - Added Header,Navbar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,14 @@
 import React from "react";
+import Navbar from "../../Components/Navbar/Navbar";
+import Header from "../../Components/Header/Header";
+import "./style-home.css";
 const Home = () => {
-  return <div>HOME</div>;
+  return (
+    <div>
+      <Navbar />
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
