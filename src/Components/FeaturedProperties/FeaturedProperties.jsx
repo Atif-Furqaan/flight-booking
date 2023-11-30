48015e08239ae1702de398efd5dd5cd4b3631f96
Atif Furqaan <atifurqaan@gmail.com>
2023-11-30 19:48:05 +0000

    Broke the images link and made it local

--- src/Components/FeaturedProperties/FeaturedProperties.jsx
+++ src/Components/FeaturedProperties/FeaturedProperties.jsx
@@ -4,11 +4,7 @@ function FeaturedProperties() {
   return (
     <div className="fp">
       <div className="fpItem">
-        <img
-          src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
-          alt=""
-          className="fpImg"
-        />
+        <img src="../Images/1.jpg" alt="" className="fpImg" />
         <span className="fpName">Aparthotel Stare Miasto</span>
         <span className="fpCity">Madrid</span>
         <span className="fpPrice">Starting from $120</span>
@@ -18,11 +14,7 @@ function FeaturedProperties() {
         </div>
       </div>
       <div className="fpItem">
-        <img
-          src="https://cf.bstatic.com/xdata/images/hotel/max1280x900/215955381.jpg?k=ff739d1d9e0c8e233f78ee3ced82743ef0355e925df8db7135d83b55a00ca07a&o=&hp=1"
-          alt=""
-          className="fpImg"
-        />
+        <img src="../Images/2.jpg" alt="" className="fpImg" />
         <span className="fpName">Comfort Suites Airport</span>
         <span className="fpCity">Austin</span>
         <span className="fpPrice">Starting from $140</span>
@@ -32,11 +24,7 @@ function FeaturedProperties() {
         </div>
       </div>
       <div className="fpItem">
-        <img
-          src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
-          alt=""
-          className="fpImg"
-        />
+        <img src="../Images/1.jpg" alt="" className="fpImg" />
         <span className="fpName">Four Seasons Hotel</span>
         <span className="fpCity">Lisbon</span>
         <span className="fpPrice">Starting from $99</span>
@@ -46,11 +34,7 @@ function FeaturedProperties() {
         </div>
       </div>
       <div className="fpItem">
-        <img
-          src="https://cf.bstatic.com/xdata/images/hotel/max1280x900/215955381.jpg?k=ff739d1d9e0c8e233f78ee3ced82743ef0355e925df8db7135d83b55a00ca07a&o=&hp=1"
-          alt=""
-          className="fpImg"
-        />
+        <img src="../Images/2.jpg" alt="" className="fpImg" />
         <span className="fpName">Hilton Garden Inn</span>
         <span className="fpCity">Berlin</span>
         <span className="fpPrice">Starting from $105</span>
@@ -62,5 +46,4 @@ function FeaturedProperties() {
     </div>
   );
 }
-
 export default FeaturedProperties;
